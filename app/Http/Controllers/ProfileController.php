fd4dbbf33bc7ae4a58d68fcb4cf6ed62bf66616c
hi-lo <7amzalaaroussi@gmail.com>
2023-02-09 23:51:49 +0100

    modify update profile method

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -47,160 +47,198 @@ class ProfileController extends Controller
 
         ]);
     }
-    public function showThisProfile($id)
+    public function showThisProfile()
     {
         $isAgent = true;
         if (auth()->guard('api')->check()) {
-            if (auth()->guard('api')->id() == $id) {
-                $profile = User::where('id', $id)->get();
-                $role = $profile[0]->roles;
-            } else {
-                return response()->json([
-                    'status' => 'failure',
-                    'message' => 'unauthorized',
-                ]);
-            }
-        } elseif (auth()->guard('agent-api')->check()) {
-            if (auth()->guard('agent-api')->id() == $id) {
-                $profile = Agent::where('id', $id)->get();
-                $role = $profile[0]->roles;
-            }
-        } elseif (auth()->guard('client')->check()) {
-            if (auth()->guard('client')->id() == $id) {
-                $profile = Client::where('id', $id)->get();
-                $isAgent = false;
-            }
-        }
-        return response()->json([
-            'status' => 'success',
-            'profile' => $profile,
-            'isAgent' => $isAgent
+            // if (auth()->guard('api')->id() == $id) {
+            $profile = $this->user;
+            //$role = $profile[0]->roles;
+            //}
+            // else {
+            //     return response()->json([
+            //         'status' => 'failure',
+            //         'message' => 'unauthorized',
+            //     ]);
+            // }
+            return response()->json([
+                'status' => 'success',
+                'profile' => $profile,
+                'isAgent' => $isAgent
 
-        ]);
+            ]);
+        } elseif (auth()->guard('agent-api')->check()) {
+            // if (auth()->guard('agent-api')->id() == $id) {
+            $profile = $this->user;
+            //$role = $profile[0]->roles;
+            //}
+            return response()->json([
+                'status' => 'success',
+                'profile' => $profile,
+                'isAgent' => $isAgent
+
+            ]);
+        } elseif (auth()->guard('client')->check()) {
+            // if (auth()->guard('client')->id() == $id) {
+            // $profile = Client::where('id', $id)->get();
+            $profile = $this->user;
+            $isAgent = false;
+            //}
+            return response()->json([
+                'status' => 'success',
+                'profile' => $profile,
+                'isAgent' => $isAgent
+
+            ]);
+        } else {
+            return response()->json([
+                'status' => 'danger',
+                'message' => 'Unauthenticated',
+            ]);
+        }
     }
-    public function updateMyProfile(Request $request, $id)
+    public function updateMyProfile(Request $request)
     {
         if (auth()->guard('api')->check()) {
-            if (auth()->guard('api')->id() == $id) {
-
-                $validate = Validator::make($request->all(), [
-                    'name' => 'string',
-                    'nom' => 'string',
-                    'prenom' => 'string',
-                    'adresse' => 'string',
-                    'ville' => 'string',
-                    'pays' => 'string',
-                    'cp' => 'integer',
-                    // 'societe' => 'string',   //if we were to update the admin's company we need to update it in all his agents
-                    // 'update agents set societe=$request->societe where agents.supervisor=auth()->guard('api')->id();
-                ]);
-                if ($validate->failed()) {
-                    return response()->json([
-                        'status' => 'failed',
-                        'message' => $validate->errors(),
-                    ]);
-                }
-                // $profile->name = $request->name;
-                // $profile->nom = $request->nom;
-                // $profile->prenom = $request->prenom;
-                // $profile->adresse = $request->adresse;
-                // $profile->ville = $request->ville;
-                // $profile->pays = $request->pays;
-                // $profile->cp = $request->cp;
-
-                // $profile->save();
-                User::whereId($id)->update([
-                    'name' => $request->name,
-                    'nom' => $request->nom,
-                    'prenom' => $request->prenom,
-                    'adresse' => $request->adresse,
-                    'ville' => $request->ville,
-                    'gsm' => $request->gsm,
-                    'pays' => strtoupper($request->pays),
-                    'cp' => $request->cp,
-                ]);
-                $profile = User::where('id', $id)->get();
+            //if (auth()->guard('api')->id() == $id) {
+            $profile = $this->user;
+            $validate = Validator::make($request->all(), [
+                'name' => 'string',
+                'nom' => 'string',
+                'prenom' => 'string',
+                'adresse' => 'string',
+                'ville' => 'string',
+                'pays' => 'string',
+                'cp' => 'integer',
+                // 'societe' => 'string',   //if we were to update the admin's company we need to update it in all his agents
+                // 'update agents set societe=$request->societe where agents.supervisor=auth()->guard('api')->id();
+            ]);
+            if ($validate->failed()) {
                 return response()->json([
-                    'status' => 'success',
-                    'message' => 'profile has been updated',
-                    'profile' => $profile,
-                ]);
-            } else {
-                return response()->json([
-                    'status' => 'failure',
-                    'message' => 'Unauthorized'
+                    'status' => 'failed',
+                    'message' => $validate->errors(),
                 ]);
             }
+            $profile->name = $request->name;
+            $profile->nom = $request->nom;
+            $profile->prenom = $request->prenom;
+            $profile->adresse = $request->adresse;
+            $profile->gsm = $request->gsm;
+            $profile->ville = $request->ville;
+            $profile->pays = $request->pays;
+            $profile->cp = $request->cp;
+
+            // $profile->save();
+            // User::whereId($id)->update([
+            //     'name' => $request->name,
+            //     'nom' => $request->nom,
+            //     'prenom' => $request->prenom,
+            //     'adresse' => $request->adresse,
+            //     'ville' => $request->ville,
+            //     'gsm' => $request->gsm,
+            //     'pays' => strtoupper($request->pays),
+            //     'cp' => $request->cp,
+            // ]);
+            $profile->save();
+            return response()->json([
+                'status' => 'success',
+                'message' => 'profile has been updated',
+                'profile' => $profile,
+            ]);
+            //}
+            //  else {
+            //     return response()->json([
+            //         'status' => 'failure',
+            //         'message' => 'Unauthorized'
+            //     ]);
+            // }
         } elseif (auth()->guard('agent-api')->check()) {
-            if (auth()->guard('agent-api')->id() == $id) {
-
-                $validate = Validator::make($request->all(), [
-                    'name' => 'string',
-                    'nom' => 'string',
-                    'prenom' => 'string',
-                    'adresse' => 'string',
-                    'ville' => 'string',
-                    'pays' => 'string',
-                    'gsm' => 'integer',
-                    'cp' => 'integer',
-                ]);
-                if ($validate->failed()) {
-                    return response()->json([
-                        'status' => 'failed',
-                        'message' => $validate->errors(),
-                    ]);
-                }
-                Agent::whereId($id)->update([
-                    'name' => $request->name,
-                    'nom' => $request->nom,
-                    'prenom' => $request->prenom,
-                    'adresse' => $request->adresse,
-                    'ville' => $request->ville,
-                    'pays' => $request->pays,
-                    'cp' => $request->cp,
-                    'gsm' => $request->gsm,
-                ]);
-                $profile = Agent::where('id', $id)->get();
+            //if (auth()->guard('agent-api')->id() == $id) {
+            $profile = $this->user;
+            $validate = Validator::make($request->all(), [
+                'name' => 'string',
+                'nom' => 'string',
+                'prenom' => 'string',
+                'adresse' => 'string',
+                'ville' => 'string',
+                'pays' => 'string',
+                'gsm' => 'integer',
+                'cp' => 'integer',
+            ]);
+            if ($validate->failed()) {
                 return response()->json([
-                    'status' => 'success',
-                    'message' => 'profile has been updated',
-                    'profile' => $profile,
+                    'status' => 'failed',
+                    'message' => $validate->errors(),
                 ]);
             }
+            $profile->name = $request->name;
+            $profile->nom = $request->nom;
+            $profile->prenom = $request->prenom;
+            $profile->adresse = $request->adresse;
+            $profile->gsm = $request->gsm;
+            $profile->ville = $request->ville;
+            $profile->pays = $request->pays;
+            $profile->cp = $request->cp;
+            // Agent::whereId($id)->update([
+            //     'name' => $request->name,
+            //     'nom' => $request->nom,
+            //     'prenom' => $request->prenom,
+            //     'adresse' => $request->adresse,
+            //     'ville' => $request->ville,
+            //     'pays' => $request->pays,
+            //     'cp' => $request->cp,
+            //     'gsm' => $request->gsm,
+            // ]);
+            // $profile = Agent::where('id', $id)->get();
+            $profile->save();
+            return response()->json([
+                'status' => 'success',
+                'message' => 'profile has been updated',
+                'profile' => $profile,
+            ]);
+            //}
         } elseif (auth()->guard('client')->check()) {
-            if (auth()->guard('client')->id() == $id) {
-
-                $isAgent = false;
-                $validate = Validator::make($request->all(), [
-                    'nom' => 'string',
-                    'prenom' => 'string',
-                    'adresse' => 'string',
-                    'ville' => 'string',
-                    'pays' => 'string',
-                    'cp' => 'integer',
-                ]);
-                if ($validate->failed()) {
-                    return response()->json([
-                        'status' => 'failed',
-                        'message' => $validate->errors(),
-                    ]);
-                }
-                Client::whereId($id)->update([
-                    'nom' => $request->nom,
-                    'prenom' => $request->prenom,
-                    'adresse' => $request->adresse,
-                    'ville' => $request->ville,
-                    'pays' => $request->pays,
-                    'cp' => $request->cp,
-                    'gsm' => $request->gsm,
-                ]);
-                $profile = Client::where('id', $id)->get();
+            //if (auth()->guard('client')->id() == $id) {
+            $profile = $this->user;
+            $isAgent = false;
+            $validate = Validator::make($request->all(), [
+                'nom' => 'string',
+                'prenom' => 'string',
+                'adresse' => 'string',
+                'ville' => 'string',
+                'pays' => 'string',
+                'cp' => 'integer',
+            ]);
+            if ($validate->failed()) {
                 return response()->json([
-                    'status' => 'success',
-                    'message' => 'profile has been updated',
-                    'profile' => $profile,
+                    'status' => 'failed',
+                    'message' => $validate->errors(),
                 ]);
             }
+
+            $profile->nom = $request->nom;
+            $profile->prenom = $request->prenom;
+            $profile->adresse = $request->adresse;
+            $profile->gsm = $request->gsm;
+            $profile->ville = $request->ville;
+            $profile->pays = $request->pays;
+            $profile->cp = $request->cp;
+            // Client::whereId($id)->update([
+            //     'nom' => $request->nom,
+            //     'prenom' => $request->prenom,
+            //     'adresse' => $request->adresse,
+            //     'ville' => $request->ville,
+            //     'pays' => $request->pays,
+            //     'cp' => $request->cp,
+            //     'gsm' => $request->gsm,
+            // ]);
+            $profile->save();
+            //$profile = Client::where('id', $id)->get();
+            return response()->json([
+                'status' => 'success',
+                'message' => 'profile has been updated',
+                'profile' => $profile,
+            ]);
         }
     }
 
